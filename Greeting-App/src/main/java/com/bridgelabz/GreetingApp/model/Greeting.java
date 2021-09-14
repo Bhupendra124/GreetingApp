462ea5e5f46fea14dd523f0f980e5d01e5e8920e
Bhupendra124 <wadekar.bhupendra6@gmail.com>
2021-09-15 01:27:27 +0530

    [bhupendra]ADD: return JSON for diff. Http method

---
+++ Greeting-App/src/main/java/com/bridgelabz/GreetingApp/model/Greeting.java
@@ -0,0 +1,20 @@
+package com.bridgelabz.GreetingApp.model;
+
+public class Greeting {
+    private final long id;
+    private String message;
+
+    public Greeting(){
+        id = 0;
+        message = "";
+    }
+    public Greeting(long id, String message) {
+        this.id = id;
+        this.message = message;
+    }
+
+    public void setMessage(String message) {
+        this.message = message;
+    }
+}
+
